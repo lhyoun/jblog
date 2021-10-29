243071341d9c97362ba553c6e3176e6ce8493539
lhy <gkdbsx@naver.com>
2021-10-29 19:51:07 +0900

    파일정리2

--- jblog03/src/main/java/com/douzone/jblog/controller/MainController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/MainController.java
@@ -1,10 +1,7 @@
 package com.douzone.jblog.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -12,22 +9,4 @@ public class MainController {
 	public String index() {
 		return "main/index";
 	}	
-	
-	@ResponseBody
-	@RequestMapping("/msg01")
-	public String message01() {
-		return "안녕";
-	}
-	
-	@ResponseBody
-	@RequestMapping("/msg02")
-	public Object message02(/*HttpServletResponse resp*/) throws Exception {
-		//resp.setContentType("application/json; charset=UTF-8");
-		//resp.getWriter().print("{\"message\":\"Hello World\"}");
-		
-		Map<String, Object> map = new HashMap<>();
-		map.put("message", "Hello World");
-		
-		return map;
-	}
 }

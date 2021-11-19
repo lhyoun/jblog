7b25c181b044c0938efcd7a6371703547120f493
lhy <gkdbsx@naver.com>
2021-11-19 11:07:14 +0900

    ver 04 1/2

--- jblog04/src/main/java/com/douzone/jblog/security/LoginInterceptor.java
+++ jblog04/src/main/java/com/douzone/jblog/security/LoginInterceptor.java
@@ -18,6 +18,8 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		System.out.println("a");
+		
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		
